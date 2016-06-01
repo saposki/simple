e703a0401d4cd929223f95ecbc5a8124a75fb16d
saposki <saposki@gmail.com>
2016-06-01 10:18:06 -0700

    optimize with streams

--- app.js
+++ app.js
@@ -6,10 +6,9 @@ var fs = require('fs');
 http.createServer(function(request, response){
 
    response.writeHead(200, {'Content-Type': 'text/html'});
-   var html = fs.readFileSync(__dirname + '/index.html')
-   response.end(html);
-
+   fs.createReadStream(__dirname + '/index.html').pipe(response);
+``
 }).listen(1337, '127.0.0.1');
 
 
-// streams and performance 
+// streams and performance
