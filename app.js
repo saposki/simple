515a6582d11841459e5a06c09d21c1bfe7958f3c
saposki <saposki@gmail.com>
2016-06-01 10:29:53 -0700

    outputing 'JSON'

--- app.js
+++ app.js
@@ -5,9 +5,12 @@ var fs = require('fs');
 
 http.createServer(function(request, response){
 
-   response.writeHead(200, {'Content-Type': 'text/html'});
-   fs.createReadStream(__dirname + '/index.html').pipe(response);
-``
+   response.writeHead(200, {'Content-Type': 'application/json'});
+   var obj = {
+      name: 'Sap',
+      location: 'SF'
+   };
+   response.end(JSON.stringify(obj))
 }).listen(1337, '127.0.0.1');
 
 
