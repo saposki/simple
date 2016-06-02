4f0a60c1264c2e0d8f979a20ac407eb2e760c4c6
saposki <saposki@gmail.com>
2016-06-01 21:02:26 -0700

    routing

--- app.js
+++ app.js
@@ -5,12 +5,22 @@ var fs = require('fs');
 
 http.createServer(function(request, response){
 
-   response.writeHead(200, {'Content-Type': 'application/json'});
-   var obj = {
-      name: 'Sap',
-      location: 'SF'
-   };
-   response.end(JSON.stringify(obj))
+   if(request.url === '/'){
+      fs.createReadStream(__dirname + '/index.html').pipe(response)
+   }
+   else if(request.url === '/api'){
+
+      response.writeHead(200, {'Content-Type': 'application/json'});
+      var obj = {
+         name: 'Sap',
+         location: 'SF'
+      };
+      response.end(JSON.stringify(obj))
+   }
+   else{
+      response.writeHead(404);
+      response.end();
+   }
 }).listen(1337, '127.0.0.1');
 
 
